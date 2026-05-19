d440d3a54a038d733c5938c0ecc264730724f299
Nadia <fluznadia@gmai.com>
2026-05-18 23:09:50 -0600

    Corrección de imágenes

--- src/pages/Equipo/Equipo.jsx
+++ src/pages/Equipo/Equipo.jsx
@@ -1,156 +1,176 @@
-import { Grid, Card, CardContent, Typography, CardMedia } from "@mui/material";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardMedia
+} from "@mui/material";
+
 import PageContainer from "../../components/common/PageContainer";
 
+// IMPORTAR IMÁGENES
 import juan from "../../assets/juan.png";
 import luz from "../../assets/luz.png";
-import syn from "../../assets/syn.png";
+import sindy from "../../assets/sindy.png";
 
 export default function Equipo() {
-    return (
-        <PageContainer title="Integrantes del equipo de trabajo">
-            <Grid container spacing={3}>
+  return (
+    <PageContainer title="Integrantes del equipo de trabajo">
 
-                <Grid item xs={12} md={6}>
-                    <Card
-                        sx={{
-                            boxShadow: 3,
-                            borderRadius: 2,
-                            transition: "0.3s",
-                            "&:hover": {
-                                boxShadow: 6,
-                            },
-                        }}
-                    >
-                        <CardMedia
-                            component="img"
-                            height="500"
-                            image={luz}
-                            alt="Luz Nadia"
-                        />
+      <Grid container spacing={3}>
 
-                        <CardContent>
-                            <Typography variant="h6">
-                                Luz Nadia Flores Domínguez
-                            </Typography>
+        {/* LUZ */}
+        <Grid item xs={12} md={6}>
+          <Card
+            sx={{
+              boxShadow: 3,
+              borderRadius: 2,
+              transition: "0.3s",
+              "&:hover": {
+                boxShadow: 6,
+              },
+            }}
+          >
 
-                            <Typography>
-                                <strong>No. Control:</strong> 2022150480327
-                            </Typography>
+            <CardMedia
+              component="img"
+              height="500"
+              image={luz}
+              alt="Luz Nadia"
+            />
 
-                            <Typography>
-                                <strong>Carrera:</strong> Ingeniería en Sistemas Computacionales
-                            </Typography>
+            <CardContent>
 
-                            <Typography>
-                                <strong>Correo institucional:</strong> 2022150480327@tesjo.edu.mx
-                            </Typography>
+              <Typography variant="h6">
+                Luz Nadia Flores Domínguez
+              </Typography>
 
-                            <Typography>
-                                <strong>Rol:</strong> Frontend
-                            </Typography>
+              <Typography>
+                <strong>No. Control:</strong> 2022150480327
+              </Typography>
 
-                            <Typography>
-                                <strong>Descripción:</strong> Se encarga del desarrollo de la interfaz de usuario y la experiencia del cliente
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+              <Typography>
+                <strong>Carrera:</strong> Ingeniería en Sistemas Computacionales
+              </Typography>
 
-                {/* Juan Carlos */}
-                <Grid item xs={12} md={6}>
-                    <Card
-                        sx={{
-                            boxShadow: 3,
-                            borderRadius: 2,
-                            transition: "0.3s",
-                            "&:hover": {
-                                boxShadow: 6,
-                            },
-                        }}
-                    >
-                        <CardMedia
-                            component="img"
-                            height="450"
-                            image={juan}
-                            alt="Juan Carlos"
-                        />
+              <Typography>
+                <strong>Correo institucional:</strong> 2022150480327@tesjo.edu.mx
+              </Typography>
 
-                        <CardContent>
-                            <Typography variant="h6">
-                                Juan Carlos Albarrán Sánchez
-                            </Typography>
+              <Typography>
+                <strong>Rol:</strong> Frontend
+              </Typography>
 
-                            <Typography>
-                                <strong>No. Control:</strong> 2022150480011
-                            </Typography>
+              <Typography>
+                <strong>Descripción:</strong> Se encarga del desarrollo de la interfaz de usuario y la experiencia del cliente
+              </Typography>
 
-                            <Typography>
-                                <strong>Carrera:</strong> Ingeniería en Sistemas Computacionales
-                            </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
 
-                            <Typography>
-                                <strong>Correo institucional:</strong> 2022150480011@tesjo.edu.mx
-                            </Typography>
+        {/* JUAN */}
+        <Grid item xs={12} md={6}>
+          <Card
+            sx={{
+              boxShadow: 3,
+              borderRadius: 2,
+              transition: "0.3s",
+              "&:hover": {
+                boxShadow: 6,
+              },
+            }}
+          >
 
-                            <Typography>
-                                <strong>Rol:</strong> Desarrollador Móvil
-                            </Typography>
+            <CardMedia
+              component="img"
+              height="450"
+              image={juan}
+              alt="Juan Carlos"
+            />
 
-                            <Typography>
-                                <strong>Descripción:</strong> Se encarga del desarrollo de la aplicación aplicando IA
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+            <CardContent>
 
-                {/* Sindy */}
-                <Grid item xs={12} md={6}>
-                    <Card
-                        sx={{
-                            boxShadow: 3,
-                            borderRadius: 2,
-                            transition: "0.3s",
-                            "&:hover": {
-                                boxShadow: 6,
-                            },
-                        }}
-                    >
-                        <CardMedia
-                            component="img"
-                            height="450"
-                            image={sindy}
-                            alt="Sindy"
-                        />
+              <Typography variant="h6">
+                Juan Carlos Albarrán Sánchez
+              </Typography>
 
-                        <CardContent>
-                            <Typography variant="h6">
-                                Sindy Pineda Nieto
-                            </Typography>
+              <Typography>
+                <strong>No. Control:</strong> 2022150480011
+              </Typography>
 
-                            <Typography>
-                                <strong>No. Control:</strong> 2022150480905
-                            </Typography>
+              <Typography>
+                <strong>Carrera:</strong> Ingeniería en Sistemas Computacionales
+              </Typography>
 
-                            <Typography>
-                                <strong>Carrera:</strong> Ingeniería en Sistemas Computacionales
-                            </Typography>
+              <Typography>
+                <strong>Correo institucional:</strong> 2022150480011@tesjo.edu.mx
+              </Typography>
 
-                            <Typography>
-                                <strong>Correo institucional:</strong> 2022150480905@tesjo.edu.mx
-                            </Typography>
+              <Typography>
+                <strong>Rol:</strong> Desarrollador Móvil
+              </Typography>
 
-                            <Typography>
-                                <strong>Rol:</strong> Backend
-                            </Typography>
+              <Typography>
+                <strong>Descripción:</strong> Se encarga del desarrollo de la aplicación aplicando IA
+              </Typography>
 
-                            <Typography>
-                                <strong>Descripción:</strong> Se encarga del desarrollo del servidor y la gestión de la base de datos.
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
 
-            </Grid>
-        </PageContainer>
-    );
+        {/* SINDY */}
+        <Grid item xs={12} md={6}>
+          <Card
+            sx={{
+              boxShadow: 3,
+              borderRadius: 2,
+              transition: "0.3s",
+              "&:hover": {
+                boxShadow: 6,
+              },
+            }}
+          >
+
+            <CardMedia
+              component="img"
+              height="450"
+              image={sindy}
+              alt="Sindy"
+            />
+
+            <CardContent>
+
+              <Typography variant="h6">
+                Sindy Pineda Nieto
+              </Typography>
+
+              <Typography>
+                <strong>No. Control:</strong> 2022150480905
+              </Typography>
+
+              <Typography>
+                <strong>Carrera:</strong> Ingeniería en Sistemas Computacionales
+              </Typography>
+
+              <Typography>
+                <strong>Correo institucional:</strong> 2022150480905@tesjo.edu.mx
+              </Typography>
+
+              <Typography>
+                <strong>Rol:</strong> Backend
+              </Typography>
+
+              <Typography>
+                <strong>Descripción:</strong> Se encarga del desarrollo del servidor y la gestión de la base de datos.
+              </Typography>
+
+            </CardContent>
+          </Card>
+        </Grid>
+
+      </Grid>
+
+    </PageContainer>
+  );
 }
